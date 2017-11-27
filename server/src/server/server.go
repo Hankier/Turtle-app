a5420154454f4257653dab72f68a86c55f6142e3
Piotr Przestrzelski <p.przestrzelski@protonmail.com>
2017-11-28 00:48:56 +0100

    log

--- server/src/server/server.go
+++ server/src/server/server.go
@@ -65,6 +65,7 @@ func (srv *Server)Start(clientPort, serverPort string)error{
 	if err == nil{
 		go srv.clientListener.Loop(srv.wg)
 		go srv.serverListener.Loop(srv.wg)
+		log.Print("Server started")
 		srv.wg.Wait()
 	}
 
