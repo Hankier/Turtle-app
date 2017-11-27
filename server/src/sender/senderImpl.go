5b9a3b5af126126e1572b1ebb78d670f9a00457a
Piotr Przestrzelski <p.przestrzelski@protonmail.com>
2017-11-27 17:01:41 +0100

    messages to single and loop method

--- server/src/sender/senderImpl.go
+++ server/src/sender/senderImpl.go
@@ -3,15 +3,18 @@ package sender
 import (
 	"net"
 	"sync"
+	"bufio"
+	"time"
 )
 
-const LOOP_TIME int = 1
+const LOOP_TIME = time.Second
 
 type SenderImpl struct{
 	socket *net.Conn
 	messages [][]byte
 	messagesMutex *sync.Mutex
 	canSend bool
+	canSendMutex *sync.Mutex
 	stopped bool
 }
 
@@ -22,13 +25,44 @@ func NewSenderImpl(socket *net.Conn)(*SenderImpl){
 	sender.messages = make([][]byte, 10)
 	sender.messagesMutex = &sync.Mutex{}
 	sender.canSend = true
+	sender.canSendMutex = &sync.Mutex{}
 	sender.stopped = false
 
 	return sender
 }
 
 func (sender *SenderImpl)Loop(wg sync.WaitGroup){
-	//TODO
+	defer wg.Done()
+
+	for !sender.stopped{
+		sender.canSendMutex.Lock()
+		if !sender.canSend{
+			sender.canSendMutex.Unlock()
+		} else {
+			sender.canSendMutex.Unlock()
+			sender.messagesMutex.Lock()
+			if len(sender.messages) > 0{
+				messagesCopy := sender.messages[:]
+				sender.messagesMutex.Unlock()
+
+				packet := messagesToSingle(messagesCopy)
+				writer := bufio.NewWriter(*sender.socket)
+				writer.Write(packet)
+				writer.Flush()
+			} else {
+				sender.messagesMutex.Unlock()
+			}
+		}
+		time.Sleep(LOOP_TIME)
+	}
+}
+
+func messagesToSingle(bytes [][]byte)([]byte){
+	result := make([]byte, 0)
+	for i := 0; i < len(bytes); i++{
+		result = append(result, bytes[i]...)
+	}
+	return result
 }
 
 func (sender *SenderImpl)Stop(){
--- server/src/sender/senderImpl_test.go
+++ server/src/sender/senderImpl_test.go
@@ -32,3 +32,17 @@ func TestSenderImpl_addSizeToMessage(t *testing.T){
 		t.Error("Expected 5 0, got ", bytes[0], " ", bytes[1])
 	}
 }
+
+func TestSenderImpl_messagesToSingle(t *testing.T) {
+	bytes := make([][]byte, 2)
+	bytes[0] = make([]byte, 1)
+	bytes[0][0] = 15
+	bytes[1] = make([]byte, 1)
+	bytes[1][0] = 14
+
+	result := messagesToSingle(bytes)
+
+	if result[0] != 15 || result[1] != 14{
+		t.Error("Expected 15 14, got ", result[0], " ", result[1])
+	}
+}
