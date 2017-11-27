68e2d9c40d4077a1e0b31f865b32916d1e66fb50
Piotr Przestrzelski <p.przestrzelski@protonmail.com>
2017-11-28 00:49:17 +0100

    server start

--- server/src/main/main.go
+++ server/src/main/main.go
@@ -1,51 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"log"
-	"encoding/binary"
-	"regexp"
-	"connectionListener"
+	"os"
+	"server"
 )
 
-
-
-
-func addSizeToMessage(message []byte) (outputMessage []byte) {
-	messageSizeArray := make([]byte, 4)
-	messageSize := uint32(cap(message))
-	binary.LittleEndian.PutUint32(messageSizeArray[0:], messageSize)
-	outputMessage = append(messageSizeArray, message...)
-	return outputMessage
-}
-
-
-func getIp(message string) (ipAddress string, newMessage []byte)  {
-	re := regexp.MustCompile("(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[0-9]+ ")
-	ipAddress = re.FindString(message)
-	fmt.Println(ipAddress)
-	reg := regexp.MustCompile(ipAddress)
-	message = reg.ReplaceAllString(message, "")
-	newMessage = []byte(message)
-
-	return ipAddress, newMessage
-}
-
-
-
-
-
-
 func main() {
-	fmt.Println("Hello")
-	clnc, err:= connectionListener.NewConnectionListener("4000", nil)//TODO handler
-	if err != nil {
-		clnc.Loop()
-	}
+	args := os.Args[1:]
 
-	clns, err := connectionListener.NewConnectionListener("4001", nil)//TODO handler
-	if err != nil {
-		clns.Loop()
-	}
+	name := args[0]
+	clientsPort := args[1]
+	serversPort := args[2]
+
+	srv := server.NewServer(name)
+	srv.Start(clientsPort, serversPort)
+
 }
