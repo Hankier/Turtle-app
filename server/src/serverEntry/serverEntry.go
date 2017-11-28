0eee8af5993204ee29fb867174c6f4cff32900ea
aougoust <kacper.augustynowicz@yahoo.com>
2017-11-28 01:24:32 +0100

    deleted constant size of publickey array for ServerEntry

--- server/src/serverEntry/serverEntry.go
+++ server/src/serverEntry/serverEntry.go
@@ -3,10 +3,10 @@ package serverEntry
 type ServerEntry struct{
 	Name string
 	Ip_port string
-	PublicKey [256]byte
+	PublicKey []byte
 }
 
-func NewServerEntry(name string, ip_port string, publicKey [256]byte)(*ServerEntry){
+func NewServerEntry(name string, ip_port string, publicKey []byte)(*ServerEntry){
 	srvEntry := new(ServerEntry)
 	srvEntry.Ip_port = ip_port
 	srvEntry.Name = name
