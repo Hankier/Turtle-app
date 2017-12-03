06c40ce5956ad6715e37bb67c76baa792cd4cac2
Piotr Przestrzelski <p.przestrzelski@protonmail.com>
2017-12-03 20:30:50 +0100

    connecting to server

--- client/src/client/client.go
+++ client/src/client/client.go
@@ -35,7 +35,7 @@ func NewClient()(*Client){
 }
 
 
-func (cli *Client)SendTo(msg message.Message)error{
+func (cli *Client)SendTo(msg *message.Message)error{
 
 	if cli.sess != nil{
 		cli.sess.Send(msg)
@@ -46,7 +46,7 @@ func (cli *Client)SendTo(msg message.Message)error{
 	}
 }
 
-func (cli *Client)SendInstantTo(msg message.Message)error{
+func (cli *Client)SendInstantTo(msg *message.Message)error{
 	if cli.sess != nil{
 		cli.sess.SendInstant(msg)
 		return nil
@@ -60,7 +60,7 @@ func (cli *Client)UnlockSending(){
 	cli.sess.UnlockSending()
 }
 
-func (cli *Client)connectToServer(name string)bool{
+func (cli *Client)ConnectToServer(name string)bool{
 	server := cli.serverList[name]
 	socket, err := net.Dial("tcp", server.Ip_port)
 	if err != nil {
@@ -74,6 +74,9 @@ func (cli *Client)connectToServer(name string)bool{
 }
 
 func (cli *Client)CreateSession(name string, socket net.Conn){
+	if cli.sess != nil{
+		cli.RemoveSession()
+	}
 	msgHandler := messageHandler.NewMessageHandlerImpl(cli, cli.clientCrypto)
 
 	sess := session.NewSession(socket, name, msgHandler, cli)
@@ -85,6 +88,7 @@ func (cli *Client)CreateSession(name string, socket net.Conn){
 
 func (cli *Client)RemoveSession(){
 	cli.sess.DeleteSession()
+	cli.sess = nil
 }
 
 func (cli *Client)GetRandomPath(length int)[]*serverEntry.ServerEntry{
