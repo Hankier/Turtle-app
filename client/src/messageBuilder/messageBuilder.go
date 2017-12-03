8d3f85c2fed146efee856b353fd77a4d34eea337
aougoust <kacper.augustynowicz@yahoo.com>
2017-12-03 01:39:00 +0100

    fixed messageBuilder imports

--- client/src/messageBuilder/messageBuilder.go
+++ client/src/messageBuilder/messageBuilder.go
@@ -2,7 +2,7 @@ package messageBuilder
 
 import (
 	"serverEntry"
-	"messageHandler"
+	"message"
 	"cryptographer"
 )
 
@@ -11,7 +11,7 @@ type MessageBuilder struct{
 	Receiver string
 	ReceiverServer string
 	Message string
-	MsgType messageHandler.TYPE
+	MsgType message.TYPE
 	EncType cryptographer.TYPE
 	MyName string
 }
@@ -24,11 +24,11 @@ func (msgb *MessageBuilder) SetReceiver(rcvr string)  {
 	msgb.Receiver = rcvr
 }
 
-func (msgb *MessageBuilder) SetReceiverServer(rcvrsrv string)  {
+func (msgb *MessageBuilder) SetReceiverServer(rcvrsrv string)(*MessageBuilder){
 	msgb.ReceiverServer = rcvrsrv
 }
 
-func(msgb *MessageBuilder) SetMsgType (p messageHandler.TYPE){
+func(msgb *MessageBuilder) SetMsgType (p message.TYPE){
 	msgb.MsgType = p
 }
 
@@ -40,7 +40,7 @@ func (msgb *MessageBuilder) SetMyName(p string)  {
 	msgb.MyName = p
 }
 
-func (msgb *MessageBuilder)Build()(*messageHandler.Message){
+func (msgb *MessageBuilder)Build()(*message.Message){
 
 	msgContent := ""
 
@@ -52,7 +52,7 @@ func (msgb *MessageBuilder)Build()(*messageHandler.Message){
 	msgContent += msgb.Receiver
 	msgContent += msgb.Message
 
-	msg := messageHandler.Message{msgb.MsgType, msgb.MyName, msgb.EncType, []byte(msgContent)}
+	msg := message.Message{msgb.MsgType, msgb.MyName, msgb.EncType, []byte(msgContent)}
 
 	return &msg
 }
