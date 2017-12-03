dbbd5754dc6b2b1498fa248648769834221aaf3a
Piotr Przestrzelski <p.przestrzelski@protonmail.com>
2017-12-03 02:07:18 +0100

    test changes

--- client/src/sender/senderImpl_test.go
+++ client/src/sender/senderImpl_test.go
@@ -2,21 +2,6 @@ package sender
 
 import "testing"
 
-func TestSenderImpl_addSizeToMessage(t *testing.T){
-	bytes := make([]byte, 5)
-	bytes[0] = 123
-	bytes[1] = 2
-	bytes[2] = 32
-	bytes[3] = 5
-	bytes[4] = 3
-
-	bytes = addSizeToMessage(bytes)
-
-	if bytes[0] != 5 || bytes[1] != 0{
-		t.Error("Expected 5 0, got ", bytes[0], " ", bytes[1])
-	}
-}
-
 func TestSenderImpl_messagesToSingle(t *testing.T) {
 	bytes := make([][]byte, 2)
 	bytes[0] = make([]byte, 1)
