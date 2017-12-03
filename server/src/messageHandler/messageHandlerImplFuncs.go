443e09831f42c016e383b05c87ea6fd91a807b93
Piotr Przestrzelski <p.przestrzelski@protonmail.com>
2017-12-03 02:00:24 +0100

    getters and setters

--- server/src/messageHandler/messageHandlerImplFuncs.go
+++ server/src/messageHandler/messageHandlerImplFuncs.go
@@ -2,38 +2,38 @@ package messageHandler
 
 import (
 	"log"
+	"message"
 )
 
-func (handler *MessageHandlerImpl)handleMSG(msg *Message){
-	if len(msg.messageContent) < 8{
+func (handler *MessageHandlerImpl)handleMSG(msg *message.Message){
+	if len(msg.GetMessageContent()) < 8{
 		log.Print("Unexpected message end")
 		return
 	}
-	nextName := string(msg.messageContent[0:8])
+	nextName := string(msg.GetMessageContent()[0:8])
 
-	msg.messageContent = append([]byte(nil), msg.messageContent[8:]...)
+	msg.SetMessageContent(append([]byte(nil), msg.GetMessageContent()[8:]...))
 
-	bytes := msg.ToBytes()
-	handler.sessSender.SendTo(nextName, bytes)
+	handler.sessSender.SendTo(nextName, msg)
 
-	msgOk := new(Message)
-	msgOk.messageType = MSG_OK
-	msgOk.messageContent = make([]byte,0)
+	msgOk := new(message.Message)
+	msgOk.SetMessageType(message.MSG_OK)
+	msgOk.SetMessageContent(make([]byte,0))
 
 	//log.Print("handleMSG, nextName: " + nextName + " msg " + string(bytes))
 
-	handler.sessSender.SendInstantTo(msg.previousName, msgOk.ToBytes())
+	handler.sessSender.SendInstantTo(msg.GetPreviousName(), msgOk)
 }
 
-func (handler *MessageHandlerImpl)handleMSG_OK(msg *Message){
-	handler.sessSender.UnlockSending(msg.previousName)
+func (handler *MessageHandlerImpl)handleMSG_OK(msg *message.Message){
+	handler.sessSender.UnlockSending(msg.GetPreviousName())
 }
 
-func (handler *MessageHandlerImpl)handlePING(msg *Message){
-	msgOk := new(Message)
-	msgOk.messageType = MSG_OK
-	msgOk.messageContent = make([]byte,0)
-	handler.sessSender.SendInstantTo(msg.previousName, msgOk.ToBytes())
+func (handler *MessageHandlerImpl)handlePING(msg *message.Message){
+	msgOk := new(message.Message)
+	msgOk.SetMessageType(message.MSG_OK)
+	msgOk.SetMessageContent(make([]byte,0))
+	handler.sessSender.SendInstantTo(msg.GetPreviousName(), msgOk)
 
 	//TODO real PING
 	log.Print("RECEIVED PING")
