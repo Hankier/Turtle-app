d1f35ccabeccdc9ad766bd2ee877f98b1e1b7593
aougoust <kacper.augustynowicz@yahoo.com>
2017-12-03 02:31:58 +0100

    message builder w/out encryption

--- client/src/messageBuilder/messageBuilder.go
+++ client/src/messageBuilder/messageBuilder.go
@@ -12,47 +12,94 @@ type MessageBuilder struct{
 	ReceiverServer string
 	Message string
 	MsgType message.TYPE
-	EncType cryptographer.TYPE
+	EncTypeServ cryptographer.TYPE
+	EncTypeCli	cryptographer.TYPE
 	MyName string
+	MyServer string
 }
 
-func (msgb *MessageBuilder)AddToPath(srve serverEntry.ServerEntry){
+func (msgb *MessageBuilder)AddToPath(srve serverEntry.ServerEntry)(*MessageBuilder){
 	msgb.Path = append(msgb.Path, srve)
+	return msgb
 }
 
-func (msgb *MessageBuilder) SetReceiver(rcvr string)  {
+func (msgb *MessageBuilder) SetReceiver(rcvr string) (*MessageBuilder) {
 	msgb.Receiver = rcvr
+	return msgb
 }
 
-func (msgb *MessageBuilder) SetReceiverServer(rcvrsrv string)  {
+func (msgb *MessageBuilder) SetReceiverServer(rcvrsrv string) (*MessageBuilder) {
 	msgb.ReceiverServer = rcvrsrv
+	return msgb
 }
 
-func(msgb *MessageBuilder) SetMsgType (p message.TYPE){
+func(msgb *MessageBuilder) SetMsgType (p message.TYPE)(*MessageBuilder){
 	msgb.MsgType = p
+	return msgb
 }
 
-func (msgb *MessageBuilder) SetEncType(p cryptographer.TYPE){
-	msgb.EncType = p
+func (msgb *MessageBuilder) SetEncTypeServ(p cryptographer.TYPE)(*MessageBuilder){
+	msgb.EncTypeServ = p
+	return msgb
 }
 
-func (msgb *MessageBuilder) SetMyName(p string)  {
+func (msgb *MessageBuilder) SetEncTypeCli(p cryptographer.TYPE)(*MessageBuilder){
+	msgb.EncTypeCli = p
+	return msgb
+}
+
+func (msgb *MessageBuilder) SetMyName(p string)(*MessageBuilder)  {
 	msgb.MyName = p
+	return msgb
+}
+
+func (msgb *MessageBuilder) SetMyServer(p string)(*MessageBuilder){
+	msgb.MyServer = p
+	return msgb
 }
 
 func (msgb *MessageBuilder)Build()(*message.Message){
 
+	msgPieces := make([][]byte, 0,0)
+	msgBytes := make([]byte, 0)
 	msgContent := ""
 
-	for _, srv := range msgb.Path{
-		msgContent += srv.Name
+
+	msgContent = msgb.Message + msgContent
+
+	msgContent = msgb.MyServer + msgb.MyName + msgContent
+
+	msgContent = msgb.Receiver
+
+
+	piece := message.Message{msgb.MsgType, nil, msgb.EncTypeCli, []byte(msgContent)}
+
+	msgPieces[0] = piece.ToBytes()
+
+	msgContent = msgb.Receiver
+
+	piece = message.Message{msgb.MsgType, nil, msgb.EncTypeServ, []byte(msgContent)}
+
+	msgPieces[1] = piece.ToBytes()
+
+	msgContent = msgb.ReceiverServer
+
+	piece = message.Message{msgb.MsgType, nil, msgb.EncTypeCli, []byte(msgContent)}
+
+	msgPieces[2] = piece.ToBytes()
+
+	for i := 3; i < len(msgb.Path); i++{
+		msgContent = msgb.Path[i].Name
+		piece = message.Message{msgb.MsgType, nil, msgb.EncTypeCli, []byte(msgContent)}
+
+		msgPieces[i] = piece.ToBytes()
 	}
 
-	msgContent += msgb.ReceiverServer
-	msgContent += msgb.Receiver
-	msgContent += msgb.Message
+	for i := len(msgPieces)-1; i >= 0; i++ {
+		msgBytes = append(msgBytes, msgPieces[i]...)
+	}
 
-	msg := message.Message{msgb.MsgType, msgb.MyName, msgb.EncType, []byte(msgContent)}
+	msg := message.Message{msgb.MsgType, msgb.MyName, msgb.EncTypeServ, msgBytes}
 
 	return &msg
 }
