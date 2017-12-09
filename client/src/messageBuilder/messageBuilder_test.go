116184af592a869e322c7b5d3cec2daaa63be12f
aougoust <kacper.augustynowicz@yahoo.com>
2017-12-09 21:36:04 +0100

    messageBuilder test prototype

--- client/src/messageBuilder/messageBuilder_test.go
+++ client/src/messageBuilder/messageBuilder_test.go
@@ -5,15 +5,36 @@ import (
 	"serverList"
 	"cryptographer"
 	"message"
+	"fmt"
+	"bytes"
 )
 
 func TestMessageBuilder_Build(t *testing.T) {
 	msgb := NewMessageBuilder(serverList.NewServerList())
 
-	comparer := message.Message{message.DEFAULT, cryptographer.PLAIN, []byte("00000000")}
+	comparer := message.Message{message.DEFAULT, cryptographer.PLAIN, []byte("00000000")}.ToBytes()
+	comparer = append(comparer, message.Message{message.DEFAULT, cryptographer.PLAIN, []byte("00000001")}.ToBytes()...)
+	comparer = append(comparer, message.Message{message.DEFAULT, cryptographer.PLAIN, []byte("00000002")}.ToBytes()...)
+	comparer = append(comparer, message.Message{message.DEFAULT, cryptographer.PLAIN, []byte("00000000")}.ToBytes()...)
+	comparer = append(comparer, message.Message{message.DEFAULT, cryptographer.PLAIN, []byte("50000000")}.ToBytes()...)
 
-
+	comparer = append(comparer, message.Message{message.DEFAULT, cryptographer.PLAIN, []byte("abcd")}.ToBytes()...)
 	msgContent := "abcd"
 
-	msgb.SetEncType(cryptographer.PLAIN).SetMsgContent()
+	path := []string{"00000002", "00000001", "00000000"}
+
+	msg,_ := msgb.SetEncType(cryptographer.PLAIN).
+		SetMsgContent([]byte(msgContent)).
+			SetMyServer("00000000").
+				SetMsgType(message.DEFAULT).
+					SetPath(path).
+						SetReceiver("50000000").
+							SetReceiverServer("00000000").
+								Build()
+	fmt.Println(msg.ToBytes())
+	fmt.Println(comparer)
+
+	if bytes.Equal(msg.ToBytes(), comparer){
+		t.Error("Unexpected message")
+	}
 }
