825de63adae23f7050ddd427460d3d1471209d17
Piotr Przestrzelski <p.przestrzelski@protonmail.com>
2017-12-14 16:58:57 +0100

    added error

--- client/src/sessions/sender.go
+++ client/src/sessions/sender.go
@@ -3,5 +3,5 @@ package sessions
 type Sender interface{
 	Send(name string, content []byte)error
 	SendInstant(name string, content []byte)error
-	UnlockSending(name string)
+	UnlockSending(name string)error
 }
