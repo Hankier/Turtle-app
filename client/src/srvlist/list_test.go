866b87ef4fd52042630acfee96b9b1f7f16cea82
Piotr Przestrzelski <p.przestrzelski@protonmail.com>
2017-12-15 21:53:31 +0100

    new test and random path

--- client/src/srvlist/list_test.go
+++ client/src/srvlist/list_test.go
@@ -3,51 +3,46 @@ package srvlist
 import (
 	"testing"
 	"math/rand"
-	"log"
+	"srvlist/entry"
+	"strconv"
 )
 
 func TestServerList_GetRandomPath(t *testing.T) {
 	sli := New()
 	var path []string
-	sli.list = make(map[string]*serverEntry)
+	sli.list = make(map[string]*entry.Entry)
 
-	path_len := 25;
+	pathLen := 25;
 
-	_, err := sli.GetRandomPath(path_len)
+	_, err := sli.GetRandomPath(pathLen)
 
-	if err != nil{
-		log.Println(err)
-	}else{
+	if err == nil{
 		t.Fail()
 	}
 
-	sli.list["0"] = New("0", "0", nil, nil)
+	sli.list["0"] = entry.New("0", "0", nil, nil)
 
 
-	_, err = sli.GetRandomPath(path_len)
+	_, err = sli.GetRandomPath(pathLen)
 
-	if err != nil{
-		log.Println(err)
-	}else{
+	if err == nil{
 		t.Fail()
 	}
 
-	sli.list["1"] = New("1", "1", nil, nil)
+	sli.list["1"] = entry.New("1", "1", nil, nil)
 
-	_, err = sli.GetRandomPath(path_len)
+	_, err = sli.GetRandomPath(pathLen)
 
 	if err != nil{
-		log.Println(err)
-	}else{
 		t.Fail()
 	}
 
 	var kstring string
 
 	for k := 2; k < 1000; k++{
-		kstring = string(k)
-		sli.list[kstring] = New(kstring, kstring, nil, nil)
-		rnd := rand.Intn(path_len)
+		kstring = strconv.Itoa(k)
+		sli.list[kstring] = entry.New(kstring, kstring, nil, nil)
+		rnd := rand.Intn(pathLen)
 		path, err = sli.GetRandomPath(rnd)
 		if rnd > 0{
 			for i := 1; i < len(path); i++{
