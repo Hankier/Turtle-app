41fc826492d85d29c55719b8cc01d782708a6331
Piotr Przestrzelski <p.przestrzelski@protonmail.com>
2017-12-17 20:47:46 +0100

    new test

--- client/src/msgs/builder/builder_test.go
+++ client/src/msgs/builder/builder_test.go
@@ -9,6 +9,7 @@ import (
 	"srvlist"
 	"client/credentials"
 	"convos/msgsBuilder"
+	"srvlist/entry"
 )
 
 type MockCredsHandler struct{
@@ -54,9 +55,15 @@ func TestMessageBuilder_Build(t *testing.T) {
 	var mcmb msgsBuilder.MessageBuilder
 	mcmb = NewMCMB()
 
-	msgb := New(srvlist.New(), mcmb, mch)
+	serverListMap := make(map[string]*entry.Entry)
+	serverListMap["00000000"] = entry.New("00000000", "127.0.0.1:8080", nil, nil)
+	serverListMap["00000001"] = entry.New("00000001", "127.0.0.1:8082", nil, nil)
+	serverListMap["00000002"] = entry.New("00000002", "127.0.0.1:8084", nil, nil)
+	serverlist := srvlist.New()
+	serverlist.SetList(serverListMap)
+	msgb := New(serverlist, mcmb, mch)
 
-	expected := ([]byte)("  00000002  00000001  recvserv  recvrecv  0000000010000000  abcd")
+	expected := ([]byte)("  00000001  00000002  recvserv  recvrecv  0000000010000000  abcd")
 	expected[0] = 0
 	expected[1] = 0
 	expected[10] = 0
@@ -89,6 +96,60 @@ func TestMessageBuilder_Build(t *testing.T) {
 	fmt.Println(string(expected))
 
 	if !bytes.Equal(msg.ToBytes(), ([]byte)(expected)){
+		t.Error("Unexpected message")
+	}
+}
+
+
+func TestMessageBuilder_BuildNoPath(t *testing.T) {
+	var mch credentials.CredentialsHolder
+	mch = NewMCH("myclient", "myserver")
+
+	var mcmb msgsBuilder.MessageBuilder
+	mcmb = NewMCMB()
+
+	serverListMap := make(map[string]*entry.Entry)
+	serverListMap["myserver"] = entry.New("myserver", "0.0.0.0:0000", nil, nil)
+	serverlist := srvlist.New()
+	serverlist.SetList(serverListMap)
+	msgb := New(serverlist, mcmb, mch)
+
+	expected := ([]byte)("  00000002  00000001  recvserv  recvrecv  0000000010000000  abcd")
+	expected[0] = 0
+	expected[1] = 0
+	expected[10] = 0
+	expected[11] = 0
+	expected[20] = 0
+	expected[21] = 0
+	expected[30] = 0
+	expected[31] = 0
+	expected[40] = 0
+	expected[41] = 0
+	expected[58] = 0
+	expected[59] = 0
+
+	msgString := "abcd"
+
+	path := []string{}
+
+	//convoBuilder := builder.New(&commonKeyProtocol.CommonKeyProtocolImpl{})
+
+	message, err :=
+		msgb.SetMsgContent([]byte(msgString)).
+			SetReceiver("myclient").
+			SetReceiverServer("myserver").
+			SetEncType(crypt.PLAIN).
+			SetMsgType(msg.DEFAULT).
+			SetPath(path).
+			SetCommand(msgString).
+			Build()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(message.ToBytes()))
+	fmt.Println(string(expected))
+
+	if !bytes.Equal(message.ToBytes(), ([]byte)(expected)){
 		t.Error("Unexpected message")
 	}
 }
